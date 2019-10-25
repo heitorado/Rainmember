a7832b2135d78d9508795acf4b6801f3aa7265ac
Rafael Mota <rma7@cin.ufpe.br>
2019-10-24 23:24:14 -0300

    Create android app

---
+++ Rainmember/app/src/main/java/br/ufpe/cin/android/rainmember/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.ufpe.cin.android.rainmember
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
