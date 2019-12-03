e96025b77186644f0267d08caa7188eeec8977b2
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2019-12-02 22:38:36 -0300

    Set up notification manager and trigger it via AlarmReceiver

--- Rainmember/app/src/main/java/br/ufpe/cin/android/rainmember/MainActivity.kt
+++ Rainmember/app/src/main/java/br/ufpe/cin/android/rainmember/MainActivity.kt
@@ -1,11 +1,14 @@
 package br.ufpe.cin.android.rainmember
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -90,6 +93,8 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+        createNotificationChannel()
+
         setUpDataWorker()
 
         LocalBroadcastManager.getInstance(applicationContext)
@@ -111,6 +116,23 @@ class MainActivity : AppCompatActivity() {
         outState.putInt(CURRENT_SCREEN, currentScreen)
 
         super.onSaveInstanceState(outState)
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(getString(R.string.channel_id), name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     private fun setUpDataWorker () {
--- Rainmember/app/src/main/java/br/ufpe/cin/android/rainmember/alarm/AlarmReceiver.kt
+++ Rainmember/app/src/main/java/br/ufpe/cin/android/rainmember/alarm/AlarmReceiver.kt
@@ -1,11 +1,16 @@
 package br.ufpe.cin.android.rainmember.br.ufpe.cin.android.rainmember.alarm
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.Toast
-
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.preference.PreferenceManager
+import br.ufpe.cin.android.rainmember.MainActivity
+import br.ufpe.cin.android.rainmember.R
 
 
 class AlarmReceiver : BroadcastReceiver() {
@@ -14,7 +19,40 @@ class AlarmReceiver : BroadcastReceiver() {
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        Toast.makeText(context.applicationContext, "Alarm Manager just ran", Toast.LENGTH_LONG).show()
         Log.d(TAG, "ALARM TRIGGERED AT: ${intent.extras!!["ALARM_TIME"]}")
+
+        var notification = buildNotification(context, intent)
+
+        with(NotificationManagerCompat.from(context.applicationContext)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(1, notification.build())
+        }
+    }
+
+    private fun buildNotification(context : Context, receivedIntent: Intent) : NotificationCompat.Builder {
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(context.applicationContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context.applicationContext, 0, intent, 0)
+
+        return NotificationCompat.Builder(context.applicationContext, context.getString(R.string.channel_id))
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle("Rainmember, rainmember!")
+            .setStyle(NotificationCompat.BigTextStyle()
+            .bigText("you asked for weather stuff reminders at ${receivedIntent.extras!!["ALARM_TIME"]}.\n" +
+                    "Quick reminders list: \n" +
+                    " - You should bring an umbrella.\n" +
+                    " - You should bring an umbrella.\n" +
+                    " - You should bring an umbrella.\n" +
+                    " - You should bring an umbrella.\n" +
+                    " - You should bring an umbrella.\n" +
+                    " - You should bring an umbrella.\n" +
+                    " - You should bring an umbrella.\n" +
+                    " - You should bring an umbrella."))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
     }
 }
