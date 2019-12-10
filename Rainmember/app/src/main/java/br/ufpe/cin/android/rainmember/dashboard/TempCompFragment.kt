c30aad2308e11bf5657739ab367c24279932d486
Heitor Carvalho <heitor.scz@gmail.com>
2019-12-10 13:00:20 -0300

    Implement Temperature Comparison component

--- Rainmember/app/src/main/java/br/ufpe/cin/android/rainmember/dashboard/TempCompFragment.kt
+++ Rainmember/app/src/main/java/br/ufpe/cin/android/rainmember/dashboard/TempCompFragment.kt
@@ -11,8 +11,11 @@ import androidx.preference.PreferenceManager
 import br.ufpe.cin.android.rainmember.R
 import br.ufpe.cin.android.rainmember.br.ufpe.cin.android.rainmember.data.room.WeatherDataDB
 import br.ufpe.cin.android.rainmember.dashboard.ChooseComparingLocationActivity
+import br.ufpe.cin.android.rainmember.data.WeatherData
+import kotlinx.android.synthetic.main.fragment_temperature_comparison.*
 import kotlinx.android.synthetic.main.fragment_temperature_comparison.view.*
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class TempCompFragment : Fragment() {
     companion object {
@@ -25,23 +28,97 @@ class TempCompFragment : Fragment() {
     ): View? {
         Log.d (TAG, "Created")
 
-        val db = WeatherDataDB.getDatabase(context!!)
-        doAsync {
-            val weatherData = db.weatherDataDAO().getLatest()
-            if(weatherData != null){
-                Log.d (TAG, weatherData.temperature.toString())
-            }
-        }
-
         val view = inflater.inflate(R.layout.fragment_temperature_comparison, container,false)
+        val cityIdPref = PreferenceManager.getDefaultSharedPreferences(context!!.applicationContext).getString(getString(R.string.location_comp_preference), getString(R.string.location_comp_preference_default_value))
 
-        var card = view.findViewById<View>(R.id.card_temperature_comparison)
+        val card = view.findViewById<View>(R.id.card_temperature_comparison)
         card.setOnClickListener {
             Log.d(TAG, "Temperature Comparison Card Clicked")
             chooseLocationToCompare()
         }
 
+        if(cityIdPref != null){
+            if(cityIdPref == getString(R.string.location_comp_preference_default_value)){
+                view.temp_comp_body_text.text = getString(R.string.temperature_comparison_no_city)
+            } else {
+                view.temp_comp_body_text.text = getString(R.string.temperature_comparison_no_data)
+            }
+        }
+
+
+        val db = WeatherDataDB.getDatabase(context!!)
+        doAsync {
+            val weatherData = db.weatherDataDAO().getLatest()
+            val weatherDataComparison = db.weatherDataDAO().getLatestComparation()
+
+            if(weatherData != null && weatherDataComparison != null){
+                uiThread {
+                    it.temp_comp_body_text.text = generateComparisonText(weatherData, weatherDataComparison)
+                }
+            }
+
+
+        }
+
         return view
+    }
+
+    private fun generateComparisonText(weatherData: WeatherData, weatherDataComparison: WeatherData): String {
+        var resText = ""
+
+        val city1 = weatherData.cityName
+        val city2 = weatherDataComparison.cityName
+        val currentTemp = weatherData.temperature.toInt()
+        val comparingTemp = weatherDataComparison.temperature.toInt()
+        val currentHumidity = weatherData.humidity.toInt()
+        val comparingHumidity = weatherDataComparison.humidity.toInt()
+
+        // Comparing temperature
+        resText += if(currentTemp > comparingTemp) {
+            if(currentTemp - comparingTemp >= 10) {
+                getString(R.string.temperature_comparison_hotter_2, city1, city2)
+            } else {
+                getString(R.string.temperature_comparison_hotter_1, city1, city2)
+            }
+        } else if ( currentTemp < comparingTemp ) {
+            if(comparingTemp - currentTemp >= 10) {
+                getString(R.string.temperature_comparison_colder_2, city1, city2)
+            } else {
+                getString(R.string.temperature_comparison_colder_1, city1, city2)
+            }
+        } else {
+            getString(R.string.temperature_comparison_equal_temp, city1, city2)
+        }
+
+        resText += "\n\n"
+
+        // Comparing humidity
+        resText += if(currentHumidity > comparingHumidity) {
+            if(currentHumidity - comparingHumidity >= 40) {
+                getString(R.string.temperature_comparison_wet_2, city1, city2)
+            } else {
+                getString(R.string.temperature_comparison_wet_1, city1, city2)
+            }
+        } else if ( currentHumidity < comparingHumidity ) {
+            if(comparingHumidity - currentHumidity >= 40) {
+                getString(R.string.temperature_comparison_dry_2, city1, city2)
+            } else {
+                getString(R.string.temperature_comparison_dry_1, city1, city2)
+            }
+        } else {
+            getString(R.string.temperature_comparison_equal_humidity, city1, city2)
+        }
+
+        resText += "\n\n"
+
+        // Comparing characters
+        resText += when {
+            city1.length > city2.length -> getString(R.string.temperature_comparison_more_letters, city1, city1.length - city2.length, city2)
+            city1.length < city2.length -> getString(R.string.temperature_comparison_less_letters, city1, city2.length - city1.length, city2)
+            else -> getString(R.string.temperature_comparison_equal_letters, city1, city2)
+        }
+
+        return resText
     }
 
     private fun chooseLocationToCompare() {
